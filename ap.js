18fe6e612b2b302058d6897b39fd06d7b6584cf1
maxwell-lorenze <33530090+maxwell-lorenze@users.noreply.github.com>
2020-05-01 20:13:10 -0700

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,29 @@
+let bouncing_balls = []
+let floating_rectangles = []
+if(frameCount< 500){
+  function preload(){
+    music = loadSound("watermelonman_HH.mp3")
+  }
+  function setup() {
+    let detailX;
+    // create a canvas that fills the browser window and attach it to your HTML
+    createCanvas(windowWidth, windowHeight).parent('p5')
+
+    // the p5 variables "width" and "height" hold the dimensions of the canvas
+}
+function mouseClicked() {
+    music.play()
+    function draw(){
+      
+
+
+    }
+
+}
+// if the window is resized, resize the canvas to match
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+}}
+else{
+  window.location.href = ''
+}
